cf21511531698e10910e74d08bb4711258f814e4
Alick Gardiner <alick.gardiner@performancehorizon.com>
2018-07-23 12:00:54 +0100

    first basic version working

--- js404.py
+++ js404.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 
-process = Popen(["phantomjs", "ph.js", "https://partnerize.bamboohr.co.uk/login.php"], stdout=PIPE)
+process = Popen(["phantomjs", "--web-security=no", "--ssl-protocol=any", "--ignore-ssl-errors=yes", "ph.js", "https://cnn.com"], stdout=PIPE,stderr=PIPE)
 (output, err) = process.communicate()
-print(output)
 exit_code = process.wait()
+print(output)
--- ph.js
+++ ph.js
@@ -1,60 +1,19 @@
+"use strict";
+var page = require('webpage').create();
 var system = require('system');
 var args = system.args;
 
-var page = require('webpage').create();
-
-page.onConsoleMessage = function(msg) { 
-    console.log('in console mesage');
-    console.log(msg); 
- }; 
-
- page.onResourceError = function(resourceError) {
-    console.error(resourceError.url + ': ' + resourceError.errorString);
+page.onResourceError = function(resourceError) {
+    if (/.js/g.test(resourceError.url)) {
+        console.log(resourceError.url)
+    }
 };
 
-page.onResourceRequested = function (request) {
-    system.stderr.writeLine('= onResourceRequested()');
-    system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
-};
- 
-page.onResourceReceived = function(response) {
-    system.stderr.writeLine('= onResourceReceived()' );
-    system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
-};
- 
-page.onLoadStarted = function() {
-    system.stderr.writeLine('= onLoadStarted()');
-    var currentUrl = page.evaluate(function() {
-        return window.location.href;
-    });
-    system.stderr.writeLine('  leaving url: ' + currentUrl);
-};
- 
-page.onLoadFinished = function(status) {
-    system.stderr.writeLine('= onLoadFinished()');
-    system.stderr.writeLine('  status: ' + status);
-};
- 
-page.onNavigationRequested = function(url, type, willNavigate, main) {
-    system.stderr.writeLine('= onNavigationRequested');
-    system.stderr.writeLine('  destination_url: ' + url);
-    system.stderr.writeLine('  type (cause): ' + type);
-    system.stderr.writeLine('  will navigate: ' + willNavigate);
-    system.stderr.writeLine('  from page\'s main frame: ' + main);
-};
-
-if (args.length === 1) {
-  console.log('URL required');
-} 
-
-else {
-    args.forEach(function(arg) {
-        console.log(arg);
-        page.open(arg, function(status) {
-            console.log(status);
-            page.render('screenshot.jpeg', {format: 'jpeg', quality: '100'});
-        });
-    });
-}
-
-phantom.exit();
+page.open(args[1], function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        page.render('test2.png');
+    }
+    phantom.exit();
+});
